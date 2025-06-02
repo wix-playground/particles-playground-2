5def01bcc02850b7d1b0d6616ba6ea10cc59f265
povilas-dev <povilast@wix.com>
2025-06-02 10:06:36 +0300

    attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/particles-playground/',
+  base: '/particles-playground-2/',
   plugins: [react()],
 });
